0d09c4ea133de702ae5a8ce0f87cccdd4ed9a622
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-02-21 10:53:42 -0600

    ts updated and html created

--- readmepreveiew/src/extension.ts
+++ readmepreveiew/src/extension.ts
@@ -1,26 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Extension activated');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "readmepreveiew" is now active!');
+    let panel: vscode.WebviewPanel | undefined = undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('readmepreveiew.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Github Readme Preview!');
-	});
+    // Function to create or show the webview panel
+    const createWebView = () => {
+        if (panel) {
+            panel.reveal(vscode.ViewColumn.Two);
+        } else {
+            panel = vscode.window.createWebviewPanel(
+                'markdownPreview',
+                'Markdown Preview',
+                vscode.ViewColumn.Two,
+                {}
+            );
 
-	context.subscriptions.push(disposable);
+            // Load HTML content into the webview
+            panel.webview.html = getWebviewContent();
+        }
+
+        // Reset when the current panel is closed
+        panel.onDidDispose(() => {
+            panel = undefined;
+        });
+    };
+
+    // Function to get HTML content for the webview
+    const getWebviewContent = (): string => {
+        const htmlPath = vscode.Uri.file(
+            path.join(context.extensionPath, 'preview.html')
+        );
+
+        const htmlContent = fs.readFileSync(htmlPath.fsPath, 'utf-8');
+
+        return htmlContent;
+    };
+
+    // Register a command to open the preview
+    let disposable = vscode.commands.registerCommand('readmepreveiew.showPreview', () => {
+        createWebView();
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
